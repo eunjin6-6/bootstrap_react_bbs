7bd4608adcc684a860595c543be0d6c9c1af298a
eunjin6-6 <dmswls616@gmail.com>
2024-11-04 13:20:54 +0900

    write함수 추가중

--- src/Write.js
+++ src/Write.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Axios from 'axios';
 
 export default class Write extends Component {
+  state={
+    isModifyMode: false,
+    title: '',
+    content: ''
+  }
+
+  write = ()=>{
+    Axios.post('http://localhost:8000/insert',{
+      title:this.state.title,
+      content:this.state.content
+    })
+    .then((res)=>{
+      console.log(res);
+    })
+    .catch((e)=>{
+      // 에러 핸들링
+      console.log(e);
+    })
+  }
+
   render() {
     return (
     <Form>
