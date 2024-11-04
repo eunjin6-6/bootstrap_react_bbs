7f5188df2bca38532d972cb8b310786d58e6eb15
eunjin6-6 <dmswls616@gmail.com>
2024-11-04 12:03:26 +0900

    Axios 설치

--- src/BoardList.js
+++ src/BoardList.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
+import Axios from 'axios';
+
+const submitTest = ()=>{
+  //react->서버로 요청을 보내고, 그 결과를 출력
+
+  // 지정된 ID를 가진 유저에 대한 요청
+  Axios.get('http://localhost:8000/')
+  .then(function (response) {
+    // 성공 핸들링
+    alert('등록 완료!');
+    console.log(response);
+  })
+  .catch(function (error) {
+    // 에러 핸들링
+    console.log(error);
+  })
+
+}
 
 
 export default class BoardList extends Component {
@@ -38,7 +56,7 @@ export default class BoardList extends Component {
           </tbody>
         </Table>
         <div className="d-flex gap-1">
-          <Button variant="primary">글쓰기</Button>{' '}
+          <Button variant="primary" onClick={submitTest}>글쓰기</Button>{' '}
           <Button variant="secondary">수정하기</Button>{' '}
           <Button variant="danger">삭제하기</Button>{' '}
         </div>
