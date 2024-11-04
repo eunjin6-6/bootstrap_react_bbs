0877b2d968ce2e0c065bffac40d0a0aa9c1f9541
eunjin6-6 <dmswls616@gmail.com>
2024-11-04 10:36:22 +0900

    ~write.js 작성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import BoardList from './BoardList';
+import Write from './Write';
 
 function App() {
   return (
     <div className="container">
       <h1 className="my-3">React Board</h1>
       <BoardList />
+      <hr />
+      <Write />
     </div>
     
   );
